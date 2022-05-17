352c45fd50a6e3011726950642e1f76d1a7b12c5
patriciamiguel <91090710+patriciamiguel@users.noreply.github.com>
2022-05-17 20:39:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+
+
+function clicou(){
+    document.getElementById("Agradecimento").innerHTML= "Obrigado por clicar";
+    //console.log(document.getElementById("Agradecimento"));
+//alert("obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://www.facebook.com/");
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML="obrigadao por passar o mouse"
+   // alert("trocar texto");
+}
+
+/*function soma(n1, n2){
+    return n1+ n2;
+}
+
+function validaidade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true
+    }else{
+    validar = false
+}
+return validar;
+}
+var idade = prompt("Qual a sua idade");
+console.log(validaidade(idade));
+
+/*alert(soma(5, 10));*/
+
+/*
+ var count = 0;
+ while (count <= 5){
+     console.log(count);
+     count = count + 1;
+ };
+
+
+/*
+var idade = prompt("Qual a sua idade")
+if (idade >= 18){
+alert("maior de idade");
+}else{
+alert("menor de idade");
+}
+
+
+
+/*
+var fruta =[{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"Roxa"}]
+console.log(fruta);
+alert(fruta[1].nome);
+*/
+
+/*
+var fruta ={nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista =[ "maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toLocaleString());
+//console.log(lista.join(" - "));
+
+
+
+
+// var nome= "Patricia Miguel";
+// var n1=5;
+//var n2=3;
+//var frase= "japão ´r o mrlhot time do mundo";
+//alert(nome + " tem " + idade = " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n2 * n2);
+//console.log(frase.toLowercase());
+//alert(frase.replace("japão", "Brasil"));
